dfc08a750744257398cf9c7928d3e61eb5620f40
Topher <topher6345@gmail.com>
2015-01-07 14:34:44 -0800

    Create 2015-1-7-Keeping-json-public-admin-prive-rails.rb

---
+++ _posts/2015-1-7-Keeping-json-public-admin-prive-rails.rb
@@ -0,0 +1,12 @@
+---
+layout: post
+status: publish
+published: true
+title: Keeping JSON API public but Admin views private in Rails
+author: Topher
+date: '2015-1-7 13:29:54 -0700'
+categories:
+- Code
+tags:
+- Ruby
+---
